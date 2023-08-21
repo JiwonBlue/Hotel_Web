f2470c06a10ecd35a2a15f1a5ce353b8809aef67
dudxoJJang <dudxo5717@naver.com>
2023-08-21 13:02:01 +0900

    리포지토리 생성

--- src/main/java/everest/hotel/repository/BoardRepository.java
+++ src/main/java/everest/hotel/repository/BoardRepository.java
@@ -1,26 +1,43 @@
 package everest.hotel.repository;
 
-import everest.hotel.domain.Board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
-    // 해당 조건을 만족하는 게시물 검색
-    List<Board> findByBoardCode(String boardCode); // board_Code
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-    // 이 두 개의 매개변수를 사용하여 해당 조건을 만족하는 게시물을 검색
-    List<Board> findByBoardCodeAndMemberId(String boardCode, String memberId); // And, Or,...
+import everest.hotel.domain.Board;
 
-    // member_id에 특정 문자열을 포함하는 모든 게시물을 조회하여 리스트로 반환
-    List<Board> findByMemberIdContaining(String memberId); // XXXContaining()은 like연산자 역할
+public interface BoardRepository extends JpaRepository<Board, String> {
 
-    // 게시물 수 조회
-    /*
-     * Long countByBoardCode(String boardCode);
-     * 
-     * // 게시물의 댓글 수 조회
-     * Long countCommentsByBoardCode(Long boardCode);
-     */
+    Board findBySeq(String seq);
+
+    Page<Board> findByOrderBySeqDesc(Pageable pageable);
+
 }
+
+BOARD_CODE VARCHAR2(10) NOT NULL CONSTRAINT BOARD_CODE_PK PRIMARY KEY,
+--게시판코드(PK)
+
+BOARD_TITLE VARCHAR2(50) NOT NULL, --게시판제목
+
+BOARD_CONTENT VARCHAR2(1000) NOT NULL, --게시판내용
+
+MEMBER_ID VARCHAR2(30) NOT NULL CONSTRAINT BOARD_MEMBER_ID_FK REFERENCES
+MEMBER_TABLE(MEMBER_ID), --회원정보CODE(FK)
+
+BOARD_VIEW NUMBER(20), --게시판조회수
+BOARD_RDATE DATE NOT NULL, --게시판작성일
+BOARD_UDATE DATE NOT NULL --게시판수정일
+
+INSERT INTO
+
+BOARD_TABLE VALUES(BOARD_TABLE_SEQ.NEXTVAL, '제목이다', '내용이다',
+'P123@NAVER.COM', 0, SYSDATE, SYSDATE);
+INSERT INTO
+
+BOARD_TABLE VALUES(BOARD_TABLE_SEQ.NEXTVAL, '이런제목', '이런내용',
+'T123@NAVER.COM', 0, SYSDATE, SYSDATE);
+INSERT INTO
+
+BOARD_TABLE VALUES(BOARD_TABLE_SEQ.NEXTVAL, '저런제목', '저런내용',
+'Y123@NAVER.COM', 0, SYSDATE, SYSDATE);
